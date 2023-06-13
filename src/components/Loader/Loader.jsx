07adf24bb732a308be96c3014a2ffb4b2117dd0f
Hanna Svietik <hsvietik@gmail.com>
2023-06-13 21:29:28 +0100

    loader centered

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,23 @@
 import ContentLoader from 'react-content-loader';
 
 export const Loader = () => (
-  <ContentLoader
-    viewBox="0 0 400 160"
-    height={160}
-    width={400}
-    backgroundColor="transparent"
+  <div
+    style={{
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    }}
   >
-    <circle cx="150" cy="86" r="8" />
-    <circle cx="194" cy="86" r="8" />
-    <circle cx="238" cy="86" r="8" />
-  </ContentLoader>
+    <ContentLoader
+      viewBox="0 0 400 160"
+      height={160}
+      width={400}
+      backgroundColor="transparent"
+    >
+      <circle cx="150" cy="86" r="8" />
+      <circle cx="194" cy="86" r="8" />
+      <circle cx="238" cy="86" r="8" />
+    </ContentLoader>
+  </div>
 );
 
 Loader.metadata = {
